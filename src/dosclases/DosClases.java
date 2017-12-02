d3dd99c159f53b6c546bda6bf6c1401225adbbd5
albracu <yo@alfredobravocuero.co>
2017-12-01 22:04:24 -0500

    Ejecutar metodo constructor.

--- src/dosclases/DosClases.java
+++ src/dosclases/DosClases.java
@@ -1,12 +1,18 @@
 package dosclases;
 
 public class DosClases {
+
+    // El Obgetivo del metodo Constructor es inicializar variables y se ejecuta automaticamente.
+    public DosClases(){
+
+        System.out.println("Metodo Constructor Ejecutado!!");
+
+    }
+
     public static void main(String[] args) {
 
-        // Instanciar la Clase NuevaClase
-        NuevaClase nueva = new NuevaClase();
-
-        nueva.mensajeconparametros("Alfredo", "Bravo Cuero");
+        // Instanciar el metodo Conxtructor
+        DosClases objeto = new DosClases();
 
 
     }
