b3010a90015029dee634a166b10ea9a619cd19ae
albracu <yo@alfredobravocuero.co>
2017-12-01 21:02:20 -0500

    Initial commit

---
+++ src/dosclases/DosClases.java
@@ -0,0 +1,13 @@
+package dosclases;
+
+public class DosClases {
+    public static void main(String[] args) {
+        // Llamar a otra Clase y otro metodo
+        NuevaClase nueva = new NuevaClase();
+
+        nueva.mensajitos();
+
+        nueva.otromensajito();
+
+    }
+}
---
+++ src/dosclases/NuevaClase.java
@@ -0,0 +1,11 @@
+package dosclases;
+
+public class NuevaClase {
+    public void mensajitos(){
+        System.out.println("Yo soy un mensajito");
+    }
+
+    public void otromensajito(){
+        System.out.println("Yo soy un Otro mensajito");
+    }
+}
