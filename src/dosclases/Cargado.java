e574d2aa186f428d6d55b1dced1b33b2a766b44a
albracu <yo@alfredobravocuero.co>
2017-12-01 23:25:25 -0500

    Sobre Carga de Constructores

---
+++ src/dosclases/Cargado.java
@@ -0,0 +1,15 @@
+package dosclases;
+
+public class Cargado {
+    public Cargado(int h){
+        System.out.println("La Hora es: "+h);
+    }
+
+    public Cargado(int h, int m){
+        System.out.println("La Hora es: "+ h + "El minuto es: "+ m);
+    }
+
+    public Cargado(int h, int m, int s){
+        System.out.println("La Hora es: "+ h + " El minuto es: "+ m + " Y el segunto es : " +s);
+    }
+}
--- src/dosclases/DosClases.java
+++ src/dosclases/DosClases.java
@@ -2,12 +2,7 @@ package dosclases;
 
 public class DosClases {
     public static void main(String[] args) {
-        // Llamar a otra Clase y otro metodo
-        NuevaClase nueva = new NuevaClase();
-
-        nueva.mensajitos();
-
-        nueva.otromensajito();
+        Cargado objeto = new Cargado(1, 4);
 
     }
 }
